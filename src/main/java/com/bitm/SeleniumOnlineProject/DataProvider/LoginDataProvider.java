bfbb0a353a9418953db4ca1b6b218dd316702762
imran <imranhassanaiub@gmail.com>
2020-07-25 21:00:01 +0600

    Data driven Testing for LoginTest

--- src/main/java/com/bitm/SeleniumOnlineProject/DataProvider/LoginDataProvider.java
+++ src/main/java/com/bitm/SeleniumOnlineProject/DataProvider/LoginDataProvider.java
@@ -1,12 +1,24 @@
 package com.bitm.SeleniumOnlineProject.DataProvider;
 
+import java.io.IOException;
+
 import org.testng.annotations.DataProvider;
 
-public class LoginDataProvider {
+import com.bitm.SeleniumOnlineProject.Utils.ExcelUtils;
 
-	@DataProvider
-	 public static Object[][] getLoginData()
-	 {
-		 
-	 }
+public class LoginDataProvider {
+	@DataProvider(name="loginData")
+	public static Object [][] getLoginData(){
+		try {
+			return new Object[][]{
+					{
+						ExcelUtils.getLoginData()
+					},
+				};
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			return null;
+		}	        
+    }
 }
--- src/main/java/com/bitm/SeleniumOnlineProject/Utils/ExcelUtils.java
+++ src/main/java/com/bitm/SeleniumOnlineProject/Utils/ExcelUtils.java
@@ -3,10 +3,18 @@ package com.bitm.SeleniumOnlineProject.Utils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.bitm.SeleniumOnlineProject.DTO.LoginDTO;
 
 public class ExcelUtils {
 
@@ -25,4 +33,44 @@ public class ExcelUtils {
 		return sheet;
 	}
 	
+	//LoginTest
+	public static List<LoginDTO> getLoginData() throws IOException
+	{
+	List<LoginDTO> logindata = new ArrayList<LoginDTO>();
+	DataFormatter formatter = new DataFormatter();
+	//Login
+	Iterator<Row> rowiterator = ExcelUtils.getSheet(0).iterator();
+	
+	while(rowiterator.hasNext())
+	{
+		Row newxtrow = rowiterator.next();
+		Iterator<Cell> celliterator = newxtrow.cellIterator();
+		
+		
+		LoginDTO login =new LoginDTO();
+		byte cellCounter =0;
+		while(celliterator.hasNext())	
+		{ 
+			Cell cell = celliterator.next();
+			
+			switch(cellCounter){
+				
+			case 0:
+				login.setUsername(formatter.formatCellValue(cell));
+				cellCounter++;
+				break;
+			case 1:
+				login.setPassword(formatter.formatCellValue(cell));
+				break;
+				
+			}
+		}
+		logindata.add(login);
+		
+	}
+	return logindata;
+	}
+	
+	
+		
 }
